6cc8aa638f36f649c5e24d0335b04e7bb786d742
Atheros <nexather@gmail.com>
2010-11-02 22:18:34 +0100

    fixed audio callback in svc client

--- src/network/svcd/client/main.c
+++ src/network/svcd/client/main.c
@@ -175,13 +175,15 @@ static int handle_receive(ENetEvent* event) {
 void send_callback(network_packet_t* packet){
 	ENetPacket* p;
 	
-	p = enet_packet_create(NULL, packet->data_len+2, 0);
-	p->data[0] = packet->time & 0xFF;
-	p->data[1] = (packet->time >> 8) & 0xFF;
-	
-	memcpy(p->data + 2, packet->data, packet->data_len);
-	
-	enet_peer_send(serverPeer, 2, p);
+	if (!got_auth) {
+		p = enet_packet_create(NULL, packet->data_len+2, 0);
+		p->data[0] = packet->time & 0xFF;
+		p->data[1] = (packet->time >> 8) & 0xFF;
+		
+		memcpy(p->data + 2, packet->data, packet->data_len);
+		
+		enet_peer_send(serverPeer, 2, p);
+	}
 }
 
 
