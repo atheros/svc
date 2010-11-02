0af5dfc24535ad73c69c4bf62652744d6dc373de
Evgeniy Vodolazskiy <waterlaz@gmail.com>
2010-11-02 19:35:12 +0200

    reduced the packet size from ridicilosly high setting to 256

--- src/libsvc/libsvc.c
+++ src/libsvc/libsvc.c
@@ -49,7 +49,7 @@ void svc_init(svc_send_callback_t send_callback){
 	svc_options = malloc(sizeof(svc_options_t));
 	svc_options->frame_size = 1024;
 	svc_options->sample_rate = 44100;
-	svc_options->byte_per_packet = 2560;
+	svc_options->byte_per_packet = 256;
 	
 	svc_encoder = encoder_create(svc_options->sample_rate, svc_options->frame_size, svc_options->byte_per_packet);
 	packet_queue = packet_queue_create(100, svc_options->frame_size);
