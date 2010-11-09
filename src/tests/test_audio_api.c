4fda0f7c65b5ee23a766abf762879f9271d45f08
Sergey Alirzaev <zl29ah@gmail.com>
2010-11-09 05:08:59 +0300

    beautify test_audio_api

--- src/tests/test_audio_api.c
+++ src/tests/test_audio_api.c
@@ -2,23 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 #include "audio_api.h"
 
+#define FRAME_SIZE 1024
 
 void callback(audio_data_t *input_packet, audio_data_t *output_packet) {
-	memcpy(output_packet->data, input_packet->data, sizeof(float) * 1024); 
+	memcpy(output_packet->data, input_packet->data, sizeof(sample_t) * FRAME_SIZE); 
+	/*
+	for (unsigned i = 0; i < FRAME_SIZE; i++) {
+		output_packet->data[i] = sinf(i / (sample_t)FRAME_SIZE * 50) - 1.0;
+	}
+	*/
 }
 
 int main() {
 	set_audio_callback(callback);
 
-	init_audio(44100, 1024);
+	init_audio(48000, FRAME_SIZE);
 
-	printf("Your mic input should be loopback to your speakers.\nEnter some text when done with test.\n");
+	puts("Your mic input should be loopback to your speakers.\nEnter some text when done with test.");
 
-	int xxx;
-	scanf("%d", &xxx);
+	getchar();
 	close_audio();
 	return 0;
 }
