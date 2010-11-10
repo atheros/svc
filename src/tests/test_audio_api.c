e5f0cee824bc087d7dfb5539f694f5b99bac41f0
Przemysław Grzywacz <nexather@gmail.com>
2010-11-10 23:43:43 +0100

    fixed a read/write error

--- src/tests/test_audio_api.c
+++ src/tests/test_audio_api.c
@@ -11,16 +11,16 @@
 audio_data_t *i_data;
 
 void read(audio_data_t *packet) {
-	memcpy(i_data->data, packet->data, sizeof(sample_t) * FRAME_SIZE);
+	memcpy(packet->data, i_data->data, sizeof(sample_t) * FRAME_SIZE); 
 }
 
 void write(audio_data_t *packet) {
-	memcpy(packet->data, i_data->data, sizeof(sample_t) * FRAME_SIZE); 
+	memcpy(i_data->data, packet->data, sizeof(sample_t) * FRAME_SIZE);
 }
 
 int main() {
 	
-	audio_data_create(FRAME_SIZE);
+	i_data = audio_data_create(FRAME_SIZE);
 
 	set_audio_callbacks(read, write);
 
