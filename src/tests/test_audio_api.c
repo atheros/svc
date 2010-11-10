3fdbb38531fbf9641a1e9ff9f0367d1e130076d7
Przemysław Grzywacz <nexather@gmail.com>
2010-11-11 00:22:14 +0100

    Fixed a stupid name colision

--- src/tests/test_audio_api.c
+++ src/tests/test_audio_api.c
@@ -6,23 +6,33 @@
 #include "audio.h"
 #include "audio_api.h"
 
+#include <assert.h>
+
 #define FRAME_SIZE 1024
 
 audio_data_t *i_data;
 
-void read(audio_data_t *packet) {
+void shit_read(audio_data_t *packet) {
+	assert(i_data != NULL);
+	assert(i_data->data != NULL);
+	assert(packet != NULL);
+	assert(packet->data != NULL);
 	memcpy(i_data->data, packet->data, sizeof(sample_t) * FRAME_SIZE);
 }
 
-void write(audio_data_t *packet) {
-	memcpy(packet->data, i_data->data, sizeof(sample_t) * FRAME_SIZE); 
+void shit_write(audio_data_t *packet) {
+	assert(i_data != NULL);
+	assert(i_data->data != NULL);
+	assert(packet != NULL);
+	assert(packet->data != NULL);
+	memcpy(packet->data, i_data->data, sizeof(sample_t) * FRAME_SIZE);
 }
 
 int main() {
 	
 	i_data = audio_data_create(FRAME_SIZE);
 
-	set_audio_callbacks(read, write);
+	set_audio_callbacks(shit_read, shit_write);
 
 	init_audio(48000, FRAME_SIZE);
 
