0835fdd3a974dca930cd71798174ac67684c7c31
Atheros <nexather@gmail.com>
2011-02-22 08:34:05 +0100

    Updated copyright/license notices in dstr and dstrutils

--- src/common/dstrutils.h
+++ src/common/dstrutils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010 Przemysław Grzywacz
+ * Copyright (c) 2010-2011 Przemysław Grzywacz
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
